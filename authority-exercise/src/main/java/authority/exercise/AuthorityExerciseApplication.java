1b1dc8d239f67a319064422d9543eff0e442e4be
yangshixiong <18234028387@sina.cn>
2020-11-24 15:46:07 +0800

    权限模块添加

---
+++ authority-exercise/src/main/java/authority/exercise/AuthorityExerciseApplication.java
@@ -0,0 +1,13 @@
+package authority.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorityExerciseApplication.class, args);
+	}
+
+}
---
+++ authority-exercise/src/test/java/authority/exercise/AuthorityExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package authority.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorityExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
