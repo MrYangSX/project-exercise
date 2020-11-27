daf9e10a94661619639f5760b84e1f27e2c1a2f2
yangshixiong <18234028387@sina.cn>
2020-11-27 15:13:15 +0800

    全局异常处理添加

---
+++ project-common/src/main/java/com/ysx/util/exception/ExceptionStatus.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.ysx.util.exception;
+
+/**
+ * 
+ * 异常状态
+ * 
+ * @author yangShiXiong
+ * @Data 2020年11月26日
+ */
+public enum ExceptionStatus {
+
+	SUCCESS(1200, "操作成功"),
+	SUCCESS_LOGGED(1203,"已登录"),
+	ERROR(1500, "操作失败"),
+	ERROR_LOGIN(1600, "登录失败"),
+	ERROR_TOKEN(1602,"TOKEN失效或者错误"),
+	ERROR_ARG(1601, "参数错误"),
+	ERROR_AUTH(1606, "认证失败"), 
+	ERROR_QUERY(1608, "数据丢失");
+
+
+    private int code;
+    private String msg;
+
+    ExceptionStatus(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
---
+++ project-common/src/main/java/com/ysx/util/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.ysx.util.exception;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ysx.util.result.ResultReturn;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * 异常处理类
+ * 全局异常处理器,处理新增的异常，对于有特殊逻辑的返回直接定义个新方法
+ * 
+ * @author yangShiXiong
+ * @Data 2020年11月26日
+ */
+
+@ControllerAdvice
+@Component
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+
+	//业务异常	
+	@ExceptionHandler(ServiceException.class)
+	public ResultReturn<String> handlerServiceException(ServiceException e){
+		log.error("业务异常:{}",e.getMessage());
+        return new ResultReturn<String>(false, Integer.getInteger(e.getCode()), null, e.getMessage(), null);
+	}
+	
+	//运行时异常
+	public ResultReturn<String> processRuntimeException(RuntimeException e) {
+        e.printStackTrace();
+        log.error("运行时异常:{}",e.getMessage());
+        return new ResultReturn<String>(false, ExceptionStatus.ERROR.getCode(), ExceptionStatus.ERROR.getMsg(), e.getMessage(), null);
+    }
+	
+	//系统异常
+	@ExceptionHandler(Exception.class)
+	public ResultReturn<String> handlerException(Exception e){
+		log.error(e.getMessage(), e);
+        return new ResultReturn<String>(false, 500, "网络异常", e.getMessage(), null);
+	}
+
+}
